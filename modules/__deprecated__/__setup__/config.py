c012ec2fd288c79fc60bac5e403c7d484d652464
sakshinilawar2003 <167522131+sakshinilawar2003@users.noreply.github.com>
2025-03-18 13:56:02 +0530

    Update config.py

--- modules/__deprecated__/__setup__/config.py
+++ modules/__deprecated__/__setup__/config.py
@@ -246,18 +246,5 @@ chatGPT_resume_chat_title = "Resume review and feedback."
 generated_resume_path = "all resumes/"
 
 ############################################################################################################
-'''
-THANK YOU for using my tool 😊! Wishing you the best in your job hunt 🙌🏻!
 
-Sharing is caring! If you found this tool helpful, please share it with your peers 🥺. Your support keeps this project alive.
-
-Support my work on <PATREON_LINK>. Together, we can help more job seekers.
-
-As an independent developer, I pour my heart and soul into creating tools like this, driven by the genuine desire to make a positive impact.
-
-Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
-
-Gratefully yours 🙏🏻,
-Sai Vignesh Golla
-'''
 ############################################################################################################
